a41904374f1ed4d5e8e031ba0c834aa947acea3d
diogo-santos04 <diogo.santos0401@gmail.com>
2024-10-11 22:44:09 -0300

    camada servico e repositorio

--- src/App.java
+++ src/App.java
@@ -1,11 +1,12 @@
 import visao.ProdutoVisao;
+
 public class App {
     public static void main(String[] args) throws Exception {
         // ClasseProdutoVisao visao = new ClasseProdutoVisao();
         // visao.exibir();
 
-        // SubclasseProdutoVisao visao = new SubclasseProdutoVisao();
-        // visao.exibir();
+        // SubclasseProdutoVisao visao2 = new SubclasseProdutoVisao();
+        // visao2.exibir();
 
         ProdutoVisao visao = new ProdutoVisao();
         visao.exibir();
---
+++ src/fakedb/ClasseProdutoFakeDB.java
@@ -0,0 +1,23 @@
+package fakedb;
+
+import dominio.ClasseProduto;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class ClasseProdutoFakeDB extends BaseFakeDB<ClasseProduto> {
+
+    public ClasseProdutoFakeDB(){
+        super();
+    }
+
+    @Override
+    protected void preencherDados() {
+        this.tabela = new ArrayList<>();
+        this.tabela.add(new ClasseProduto(1, "Bebidas", LocalDate.now()));
+        this.tabela.add(new ClasseProduto(2, "Carnes", LocalDate.now()));
+        this.tabela.add(new ClasseProduto(3, "Limpeza", LocalDate.now()));
+        this.tabela.add(new ClasseProduto(4, "Laticínios", LocalDate.now()));
+        this.tabela.add(new ClasseProduto(5, "Higiene", LocalDate.now()));
+    }
+
+}
